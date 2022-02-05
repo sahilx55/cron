2d692212e9a6f7c518d3c9a03942ad53c82bc30e
sahilnoliax <82576332+sahilnoliax@users.noreply.github.com>
2022-02-05 15:56:05 +0530

    Update t.py

--- t.py
+++ t.py
@@ -1,11 +1,19 @@
 import threading
 import requests
+from datetime import datetime
+from pytz import timezone
 bot = ['https://classroombysahil.nolia.repl.co/', 'https://ctest.nolia.repl.co/']
+
+tz = timezone('Asia/Kolkata')
+s = datetime.now(tz).strftime("%H:%M")
+sx = datetime.strptime(s, "%H:%M")
+z ='Date : ' + datetime.now(tz).strftime("%A %d-%m-%Y") + '\nTime : ' + sx.strftime("%r")
 
 def cronjob():
       threading.Timer(3.0, cronjob).start()
       for i in range(len(bot)):
           A = requests.get(bot[i])
+          print(z)
           if not A.ok:
             print(bot[i])
             print('BOT IS DEAD')
