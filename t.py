0f5bd4fd2e61775cd1c698d24883bc475f8f515b
sahilnoliax <82576332+sahilnoliax@users.noreply.github.com>
2022-02-05 15:47:26 +0530

    Update t.py

--- t.py
+++ t.py
@@ -1,11 +1,16 @@
-# import requests module
+import threading
 import requests
-  
-# Making a get request
-response = requests.get('https://classroombysahil.nolia.repl.co/')
-  
-# print response
-print(response)
-  
-# print url
-print(response.content)
+bot = ['https://classroombysahil.nolia.repl.co/', 'https://ctest.nolia.repl.co/']
+
+def cronjob():
+      threading.Timer(5.0, cronjob).start()
+      for i in range(len(bot)):
+          A = requests.get(bot[i])
+          if not A.ok:
+            print(bot[i])
+            print('BOT IS DEAD')
+          else:
+            print(bot[i])
+            print(A)
+
+cronjob()
