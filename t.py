55a44997db2c0985d9ff7fe6085b15322394f75b
sahilnoliax <82576332+sahilnoliax@users.noreply.github.com>
2022-02-05 16:15:45 +0530

    Update t.py

--- t.py
+++ t.py
@@ -4,13 +4,12 @@ from datetime import datetime
 from pytz import timezone
 bot = ['https://classroombysahil.nolia.repl.co/', 'https://ctest.nolia.repl.co/']
 
-tz = timezone('Asia/Kolkata')
-s = datetime.now(tz).strftime("%H:%M")
-sx = datetime.strptime(s, "%H:%M")
-z ='Date : ' + datetime.now(tz).strftime("%A %d-%m-%Y") + '\nTime : ' + sx.strftime("%r")
-
 def cronjob():
       threading.Timer(3.0, cronjob).start()
+      tz = timezone('Asia/Kolkata')
+      s = datetime.now(tz).strftime("%H:%M")
+      sx = datetime.strptime(s, "%H:%M")
+      z ='Date : ' + datetime.now(tz).strftime("%A %d-%m-%Y") + '\nTime : ' + sx.strftime("%r")
       print(z)
       for i in range(len(bot)):
           A = requests.get(bot[i])
