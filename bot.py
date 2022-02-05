9a2eadbd2352f43e67f062fc7ac90856db2e27fa
sahilnoliax <82576332+sahilnoliax@users.noreply.github.com>
2022-02-06 01:37:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -74,6 +74,22 @@ def cronjob():
             ftx2 = open("response.txt", "w")
             ftx2.write(repr(l2))
             ftx2.close()
+            mnb34x19 = open('check.txt', 'r')
+            mnbxop19 = mnb34x19.read()
+            mnb34x1 = open('response.txt', 'r')
+            mnbxop1 = mnb34x1.read()
+            ct1 = eval(mnbxop1)
+            ct2 = getList(ct1)
+            ct3 = '\n\n----------------\n\n'.join([f'🤖 BOT URL {i+1}: ' + '[' + ct2[i].replace(' Bot Is Alive', '') + '] 🤖\n' + '🟢 Current Status : ALIVE\n\n' + '😵 Dead On: \n' + eval(ct1[ct2[i]].replace('\n', '\\n'))[0] + '\n\n💪 Alive On: \n' + eval(ct1[ct2[i]].replace('\n', '\\n'))[1]
+                  if 'Alive' in ct2[i]
+                  else 
+                  f'🤖 BOT URL {i+1}: ' + '[' + ct2[i].replace(' Bot Is Dead', '') + '] 🤖\n' + '🟡 Current Status : DEAD\n\n' + '💪 Alive On: \n' + eval(ct1[ct2[i]].replace('\n', '\\n'))[0] + '\n\n😵 Dead On: \n' + eval(ct1[ct2[i]].replace('\n', '\\n'))[1]
+                  for i in range(len(ct2))])
+            logx = f"Last Check On : \n{mnbxop19}\n\n" + ct3
+            ftx2c = open("logs.txt", "w")
+            ftx2c.write(logx)
+            ftx2c.close()
+            await client.send_document(chat_id=message.chat.id, document="logs.txt")
           if A.ok and alive not in ct1:
             app.send_message(chat_id=-1001184862744, text=f'{bot[i]} Is Alive\n\n{z}', disable_web_page_preview=True)
             if dead not in ct1:
@@ -87,6 +103,22 @@ def cronjob():
             ftx2 = open("response.txt", "w")
             ftx2.write(repr(l2))
             ftx2.close()
+            mnb34x19 = open('check.txt', 'r')
+            mnbxop19 = mnb34x19.read()
+            mnb34x1 = open('response.txt', 'r')
+            mnbxop1 = mnb34x1.read()
+            ct1 = eval(mnbxop1)
+            ct2 = getList(ct1)
+            ct3 = '\n\n----------------\n\n'.join([f'🤖 BOT URL {i+1}: ' + '[' + ct2[i].replace(' Bot Is Alive', '') + '] 🤖\n' + '🟢 Current Status : ALIVE\n\n' + '😵 Dead On: \n' + eval(ct1[ct2[i]].replace('\n', '\\n'))[0] + '\n\n💪 Alive On: \n' + eval(ct1[ct2[i]].replace('\n', '\\n'))[1]
+                  if 'Alive' in ct2[i]
+                  else 
+                  f'🤖 BOT URL {i+1}: ' + '[' + ct2[i].replace(' Bot Is Dead', '') + '] 🤖\n' + '🟡 Current Status : DEAD\n\n' + '💪 Alive On: \n' + eval(ct1[ct2[i]].replace('\n', '\\n'))[0] + '\n\n😵 Dead On: \n' + eval(ct1[ct2[i]].replace('\n', '\\n'))[1]
+                  for i in range(len(ct2))])
+            logx = f"Last Check On : \n{mnbxop19}\n\n" + ct3
+            ftx2c = open("logs.txt", "w")
+            ftx2c.write(logx)
+            ftx2c.close()
+            await client.send_document(chat_id=message.chat.id, document="logs.txt")
 
 if __name__ == "__main__":
     try:
